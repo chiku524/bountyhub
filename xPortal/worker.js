24ff78c306120ac94bc270ed531c48e298fc098d
Nicholas Chikuji <chiku524@icloud.com>
2025-06-20 14:39:44 -0400

    updated worker.js to use createRequestHandler

--- xPortal/worker.js
+++ xPortal/worker.js
@@ -1,7 +1,11 @@
-import * as remix from './build/index.js';
+import * as build from './build/index.js';
+import { createRequestHandler } from '@remix-run/cloudflare';
 
 export default {
-  async fetch(request, env, ctx) {
-    return remix.entry.server(request, env, ctx);
+  fetch(request, env, ctx) {
+    return createRequestHandler({
+      build,
+      mode: env.NODE_ENV,
+    })(request, env, ctx);
   }
 }; 
